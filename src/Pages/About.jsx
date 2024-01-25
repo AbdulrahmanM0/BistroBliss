33f2102bdecb48e436eb10915e35a479017de7ac
hazem <hozebra518@gmail.com>
2024-01-25 13:52:01 +0200

    create GuestInfoSection

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import VisitUsSection from '../Components/PagesSup/About/VisitUsSection/VisitUsSection'
-import VideoSection from '../Components/PagesSup/About/VideoSection/VideoSection'
+import React from "react";
+import VisitUsSection from "../Components/PagesSup/About/VisitUsSection/VisitUsSection";
+import VideoSection from "../Components/PagesSup/About/VideoSection/VideoSection";
+import GuestInfoSection from "../Components/PagesSup/About/GuestInfoSection/GuestInfoSection";
 
 export default function About() {
   return (
     <div>
       <VisitUsSection />
       <VideoSection />
+      <GuestInfoSection />
     </div>
-  )
+  );
 }
